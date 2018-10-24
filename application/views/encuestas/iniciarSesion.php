ae0a6dcd43241535f822639f4b90d6f2c3b97dc8
Alfonso6z <agzdn666@gmail.com>
2018-10-24 13:13:25 -0500

    Cambie boton inicio sesion

--- application/views/encuestas/iniciarSesion.php
+++ application/views/encuestas/iniciarSesion.php
@@ -30,7 +30,7 @@
       		<?= form_input($nombre) ?></h5>
       		<h5 class = "text-center"><?=  form_label('Contraseña: ','contrasena') ?>
       		<?= form_password($contrasena) ?></h5>
-      		<h5 class = "text-right"><?= form_submit('','Iniciar',"class='btn btn-danger'")?>
+      		<h5 class = "text-center"><?= form_submit('','Iniciar',"class='btn btn-info'")?>
 			<?= form_close() ?></h5>
       </div>
       <div class="text-right">
