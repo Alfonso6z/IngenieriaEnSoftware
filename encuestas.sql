d76336a6dbd888ee1f16277a74932fa595c07ddf
Alfonso6z <agzdn666@gmail.com>
2018-11-05 12:44:18 -0600

    base de datos

--- encuestas.sql
+++ encuestas.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: localhost:3306
--- Tiempo de generación: 02-11-2018 a las 17:47:14
+-- Tiempo de generación: 05-11-2018 a las 12:43:30
 -- Versión del servidor: 5.7.24-0ubuntu0.18.04.1
 -- Versión de PHP: 7.2.10-0ubuntu0.18.04.1
 
